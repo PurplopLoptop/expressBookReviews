5e1efad646441f92bf349b2b9823b27951e9742c
PurplopLoptop <161166932+PurplopLoptop@users.noreply.github.com>
2024-06-05 12:14:56 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,18 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function searchByIsbn(isbn, bookData){
+    const results = [];
+    for(const bookId in bookData) {
+        if (bookData.hasOwnProperty(bookId)) {
+            const book = bookData[bookId];
+            if (book.isbn === isbn) {
+                results.push(book);
+            }
+        }
+    }
+    return results;
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,16 +24,16 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+    const isbn = req.params.isbn;
+    const matchingBooks = searchByIsbn(isbn, books);
+    res.json(matchingBooks);
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
